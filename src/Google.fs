98d27482ebfeee70bfeaebbe3e1be3381543f898
Marcus Griep <mgriep@cimpress.com>
2017-04-27 11:37:21 -0400

    Initial commit

---
+++ src/Google.fs
@@ -0,0 +1,10 @@
+namespace Fable.Import.Google.Cloud
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+
+type [<Erase>] ProjectId = ProjectId of string
+
+type [<Measure>] millisecond
+type [<Measure>] ms = millisecond
